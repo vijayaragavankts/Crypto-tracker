8367440b27388fe3e9d7f3eada2c94cfb8d972bc
vijayaragavan <vijayaragavan@gmail.com>
2023-06-22 21:48:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Homepage from "./Pages/Homepage";
+import CoinPage from "./Pages/CoinPage";
+import { makeStyles } from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles( () => ( {
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+} ) );
+
+function App () {
+  const classes = useStyles();
+
+  return ( <
+        BrowserRouter >
+    <
+        div className={ classes.App } >
+      <
+        Header />
+      <
+        Routes >
+        <
+          Route path="/"
+          element={ < Homepage /> }
+        /> <
+          Route path="/coins/:id"
+          element={ < CoinPage /> }
+        /> <
+        /Routes> <
+        /div> <
+        /BrowserRouter>
+        );
 }
-
-export default App;
+        export default App;
